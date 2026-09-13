ae0b5b4d36706a5294f0008668b2019a27e6bad7
Trần Hữu Hà <huuha205@gmail.com>
2026-09-14 02:12:59 +0700

    feat(dao): complete TODO 0.3 EmployeeDAO save

---
+++ Code/Slot2/Chapter1_JPA_Ex1/src/main/java/fe/de201028/dao/EmployeeDAO.java
@@ -0,0 +1,36 @@
+package fe.de201028.dao;
+
+import fe.de201028.pojo.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EmployeeDAO {
+
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("hsf302PU");
+
+    public void save(Employee e) {
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            em.persist(e);
+
+            em.getTransaction().commit();
+
+        } catch (RuntimeException ex) {
+
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw ex;
+
+        } finally {
+            em.close();
+        }
+    }
+}
