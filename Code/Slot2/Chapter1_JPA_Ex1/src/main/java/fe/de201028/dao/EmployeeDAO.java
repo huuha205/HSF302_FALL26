67af318029da86d2c67795e3d47887c2958a686c
Trần Hữu Hà <huuha205@gmail.com>
2026-09-14 02:15:49 +0700

    feat(dao): complete TODO 0.4 EmployeeDAO read

--- Code/Slot2/Chapter1_JPA_Ex1/src/main/java/fe/de201028/dao/EmployeeDAO.java
+++ Code/Slot2/Chapter1_JPA_Ex1/src/main/java/fe/de201028/dao/EmployeeDAO.java
@@ -4,6 +4,7 @@ import fe.de201028.pojo.Employee;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import java.util.List;
 
 public class EmployeeDAO {
 
@@ -33,4 +34,32 @@ public class EmployeeDAO {
             em.close();
         }
     }
+
+    public Employee findById(Long id) {
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.find(Employee.class, id);
+
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Employee> findAll() {
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.createQuery(
+                    "SELECT e FROM Employee e",
+                    Employee.class
+            ).getResultList();
+
+        } finally {
+            em.close();
+        }
+    }
+
 }
