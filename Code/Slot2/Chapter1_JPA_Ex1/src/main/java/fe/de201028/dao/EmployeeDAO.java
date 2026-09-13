1956c8f50bc67b45241efcc5efc7947924782ba5
Trần Hữu Hà <huuha205@gmail.com>
2026-09-14 02:17:37 +0700

    feat(dao): complete TODO 0.5 conditional queries

--- Code/Slot2/Chapter1_JPA_Ex1/src/main/java/fe/de201028/dao/EmployeeDAO.java
+++ Code/Slot2/Chapter1_JPA_Ex1/src/main/java/fe/de201028/dao/EmployeeDAO.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import java.util.List;
+import fe.de201028.pojo.Gender;
+import java.math.BigDecimal;
 
 public class EmployeeDAO {
 
@@ -56,6 +58,40 @@ public class EmployeeDAO {
                     "SELECT e FROM Employee e",
                     Employee.class
             ).getResultList();
+
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Employee> findByGender(Gender gender) {
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.createQuery(
+                            "SELECT e FROM Employee e WHERE e.gender = :gender",
+                            Employee.class
+                    )
+                    .setParameter("gender", gender)
+                    .getResultList();
+
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Employee> findBySalaryGreaterThan(BigDecimal salary) {
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.createQuery(
+                            "SELECT e FROM Employee e WHERE e.salary > :salary",
+                            Employee.class
+                    )
+                    .setParameter("salary", salary)
+                    .getResultList();
 
         } finally {
             em.close();
