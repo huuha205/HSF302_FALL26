304b7bc56b5752087a3686dd54fbbde19a907bfd
Trần Hữu Hà <huuha205@gmail.com>
2026-09-15 16:52:29 +0700

    feat(dao): complete TODO 2.5 Department and Employee DAO

--- Slot3/Chapter1_JPA_Mapping/src/main/java/fu/de201028/Main.java
+++ Slot3/Chapter1_JPA_Mapping/src/main/java/fu/de201028/Main.java
@@ -1,17 +1,9 @@
-package fu.de201028;
+import fu.de201028.dao.DepartmentDAO;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        DepartmentDAO dao = new DepartmentDAO();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("DAO connected successfully!");
     }
 }
