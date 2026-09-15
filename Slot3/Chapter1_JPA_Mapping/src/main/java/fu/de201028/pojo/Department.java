cb4c013a8cd0f4823e724664d3984b76885107db
Trần Hữu Hà <huuha205@gmail.com>
2026-09-15 16:15:59 +0700

    feat(entity): complete TODO 2.1 Department Employee Gender

---
+++ Slot3/Chapter1_JPA_Mapping/src/main/java/fu/de201028/pojo/Department.java
@@ -0,0 +1,44 @@
+package fu.de201028.pojo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String location;
+
+    public Department() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
