a664623e33a7349fd40be9c0e6f5d276a9484f7c
Trần Hữu Hà <huuha205@gmail.com>
2026-09-17 15:44:48 +0700

    feat(mapping): complete TODO 2.6 JOIN FETCH

--- Slot3/Chapter1_JPA_Mapping/src/main/java/fu/de201028/Main.java
+++ Slot3/Chapter1_JPA_Mapping/src/main/java/fu/de201028/Main.java
@@ -1,9 +1,20 @@
 import fu.de201028.dao.DepartmentDAO;
+import fu.de201028.pojo.Department;
+import fu.de201028.pojo.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        DepartmentDAO dao = new DepartmentDAO();
 
-        System.out.println("DAO connected successfully!");
+        DepartmentDAO deptDAO = new DepartmentDAO();
+
+        Department dept = deptDAO.findByIdWithEmployees(2L);
+
+        System.out.println("Department: " + dept.getName());
+
+        for (Employee e : dept.getEmployees()) {
+            System.out.println(
+                    e.getId() + " - " + e.getFullName()
+            );
+        }
     }
 }
--- Slot3/Chapter1_JPA_Mapping/src/main/java/fu/de201028/dao/DepartmentDAO.java
+++ Slot3/Chapter1_JPA_Mapping/src/main/java/fu/de201028/dao/DepartmentDAO.java
@@ -111,4 +111,19 @@ public class DepartmentDAO {
             em.close();
         }
     }
+
+    public Department findByIdWithEmployees(Long id) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.createQuery(
+                            "SELECT d FROM Department d JOIN FETCH d.employees WHERE d.id = :id",
+                            Department.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+
+        } finally {
+            em.close();
+        }
+    }
 }
